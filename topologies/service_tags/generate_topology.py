ae0dcd9fe506097a2fe132e1c092d7bc9405a212
Ashish Hanwadikar <ashish@kloudfuse.com>
2023-03-30 22:43:52 -0700

    Improved topology generation

--- topologies/service_tags/generate_topology.py
+++ topologies/service_tags/generate_topology.py
@@ -1,64 +1,107 @@
 import json
 import random
-from dataclasses import dataclass
 
-maxRoutes = 10
-
-@dataclass
-class Service:
-    name: str
-    instances: list
-    routes: list
-    downStreamCalls: list
 
 def generateServiceInstanceName(serviceName, n):
     return serviceName + "-instance-" + str(n)
 
-def generateRoutes(maxRoutes):
-    return ["/route" + str(routeNum) for routeNum in range(1, maxRoutes)]
 
-routes = generateRoutes(maxRoutes)
+def generateCallGraph(serviceRoutes, numServices, fanOut, maxDepth):
+    services = []
+    nextLevel = 1
+    numServicesPerLevel = int(numServices/maxDepth)
+    numRoutesPerService = 2
+    nextLevelRoutes = [(route, service) for route in range(0, numRoutesPerService) for service in range(0, numServicesPerLevel)]
+    nextLevelRoutes.reverse()
+    while nextLevel < maxDepth:
+        nextServiceRoutes = {}
+        for (service, routes) in serviceRoutes.items():
+            routesJson = []
+            for route in routes:
+                downStreamCallsJson = {}
+                routesGenerated = 0
+                while routesGenerated < fanOut and len(nextLevelRoutes) > 0:
+                    (nextServiceRouteNum, nextServiceNum) = nextLevelRoutes.pop()
+                    nextService = 'service-' + str(nextLevel) + '-' + str(nextServiceNum)
+                    nextRoute = nextService + '-route-' + str(nextServiceRouteNum)
+                    downStreamCallsJson[nextService] = nextRoute
+                    if nextService not in nextServiceRoutes:
+                        nextServiceRoutes[nextService] = {}
+                    nextServiceRoutes[nextService][nextRoute] = {}
+                    routesGenerated += 1
+                routesJson.append(
+                    {
+                        'route': route,
+                        'downstreamCalls': downStreamCallsJson,
+                        'maxLatencyMillis': random.randrange(10, 100),
+                        'tagSets': [{
+                            'tags': {
+                                'version': 'version-1',
+                            }
+                        }]
+                    }
+                )
+            services.append({
+                'serviceName': service,
+                'instances': [generateServiceInstanceName(service, n) for n in range(0, 5)],
+                'routes': routesJson,
+            })
 
-def generateServiceName(serviceNamePrefix, n):
-    return serviceNamePrefix + "-" + str(n)
+        nextLevelRoutes = [(route, service) for route in range(0, numRoutesPerService) for service in
+                           range(0, numServicesPerLevel)]
+        nextLevelRoutes.reverse()
+        serviceRoutes = nextServiceRoutes
+        nextLevel = nextLevel + 1
 
-def generateService(serviceNamePrefix, n, routes):
-    serviceName = generateServiceName(serviceNamePrefix, n)
-    return Service(name=serviceName, routes=routes, downStreamCalls=[], instances=[generateServiceInstanceName(serviceName, m) for m in range(0, 10)])
+    # last level
+    for (service, routes) in serviceRoutes.items():
+        routesJson = []
+        for route in routes:
+            routesJson.append(
+                {
+                    'route': route,
+                    'downstreamCalls': {},
+                    'maxLatencyMillis': random.randrange(10, 100),
+                    'tagSets': [{
+                        'tags': {
+                            'version': 'version-1',
+                        }
+                    }]
+                }
+            )
+        services.append({
+            'serviceName': service,
+            'instances': [generateServiceInstanceName(service, n) for n in range(0, 5)],
+            'routes': routesJson,
+        })
+    return services
 
-def generateServices(serviceNamePrefix, nServices):
-    return [generateService(serviceNamePrefix, serviceNum, routes) for serviceNum in range(1, nServices)]
 
-
-def generateDownstreamCalls(services, maxCalls):
-    for i in range(0, len(services)):
-        for j in range(0, len(routes)):
-            downStreamCalls = {}
-            for k in range(0, maxCalls):
-                i1 = random.randrange(i, len(services))
-                j1 = random.randrange(0, len(routes))
-                downstreamService = services[i1]
-                downstreamRoute = routes[j1]
-                downStreamCalls[downstreamService.name] = downstreamRoute
-            services[j].downStreamCalls.append(downStreamCalls)
-
-
-def toRouteJson(service, maxLatencyMillis=100):
-    routeJson = []
-    for i in range(0, len(service.routes)):
-        routeJson.append({'route': service.routes[i], 'downstreamCalls': service.downStreamCalls[i], 'maxLatencyMillis': random.randrange(10, maxLatencyMillis)})
-    return routeJson
-
-def toTopologyJson(services, maxTracesPerHour=1000):
-    servicesJson = [ { 'serviceName': service.name, 'instances': service.instances, 'routes': toRouteJson(service) } for service in services]
-    rootService = services[0]
-    return { 'topology': { 'services': servicesJson }, 'rootRoutes': [ {'service': rootService.name, 'route': route, 'tracesPerHour': random.randrange(100, maxTracesPerHour) } for route in rootService.routes] }
 
 def main():
-    services = generateServices("test", 10)
-    generateDownstreamCalls(services, 5)
-    print(services)
-    print(json.dumps(toTopologyJson(services), indent=2))
+    root = {
+        "service-0":
+            {
+                "route-0": {},
+                "route-1": {}
+            },
+    }
+    callGraph = generateCallGraph(root, 12, 2, 4)
+    topology = {
+        'topology': {
+            'services': callGraph,
+        },
+        'rootRoutes': [
+            {
+                'service': service,
+                'route': route,
+                'tracesPerHour': random.randrange(100, 10000)
+            }
+            for (service, routes) in root.items()
+            for route in routes
+        ]
+    }
+    print(json.dumps(topology, indent=2))
 
 
 if __name__ == "__main__":
